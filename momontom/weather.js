c0c64ff1d2eb97da14776e8405088c0b76936f35
eunjin0212 <eunjin9639@gmail.com>
2020-09-01 13:35:17 +0900

    finish

--- momontom/weather.js
+++ momontom/weather.js
@@ -1,6 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "4658b2072b0daa8e1e75d5bbd6358604";
 const COORDS = "coords";
 
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
@@ -13,6 +28,7 @@ function handleGeoSucces(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,6 +44,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoord();
   } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
     //get weather
   }
 }
